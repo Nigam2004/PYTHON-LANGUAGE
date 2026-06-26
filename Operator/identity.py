fa69c359a8bd1b9350eebbd33421fea8a5f4c3ce
Nigam Bisoyi <nigambisoyi2004@gmail.com>
2026-06-26 12:06:03 +0530

    operator_ide_memb

--- Operator/identity.py
+++ Operator/identity.py
@@ -1,3 +1,15 @@
 ##identity 
+# is- check value or  location
+# is not - check value or  location
 
-# is ,is not
+a=10
+b=10
+print(a is b)  #ans=True
+
+d=10
+c=d
+print(d is c)  #ans=True
+
+x=10
+y=20
+print(x is not y) #ans=True
--- Operator/membership.py
+++ Operator/membership.py
@@ -1,2 +1,11 @@
 ##membership 
 # in, not in
+
+student=["nigam","rajesh","sada","raja"]
+print("nigam"in student)
+print("raj"not  in student)
+
+"""its output wil be boolian
+True
+True"""
+
