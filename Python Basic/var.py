62d43e3212db35e271c44dfe839ef4960608c85e
Nigam Bisoyi <nigambisoyi2004@gmail.com>
2026-06-26 11:49:18 +0530

    python_basic_var

---
+++ Python Basic/var.py
@@ -0,0 +1,15 @@
+name="Nigam" #character/txt
+age=20  # numeric
+height=5.6 #numeric
+is_learn=True #boolian
+print(name,age,height,is_learn)
+
+print(type(is_learn)) #type() basicaly used for to know the data type
+
+a=input()  #input() basicaly used for input data and all the inputed data is string
+print(type(a))
+
+a= int(input())   #int() basicaly used for convert the any data type into numeric/intiger
+print(type(a))
+
+
