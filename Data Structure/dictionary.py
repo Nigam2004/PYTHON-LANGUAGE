5142755868cb1a4f69848ce977b16129f2229811
Nigam Bisoyi <nigambisoyi2004@gmail.com>
2026-06-28 17:40:27 +0530

    update_dictionary

--- Data Structure/dictionary.py
+++ Data Structure/dictionary.py
@@ -1,3 +1,13 @@
+childrens={}
+childrens["name"]="Jeon"
+childrens["age"]=25 
+childrens["gender"]="m" 
+childrens["father_name"]="Ram" 
+childrens["mother_name"]="Urmila" 
+print(childrens.pop("name"))
+
+"""ans -Jeon"""
+
 student={
     "name":["nigam","ram","dam","sam"],
     "roll_no":20,
@@ -7,3 +17,6 @@ student={
 }
 
 print(student["name"][1])
+"""ans -ram"""
+print(student.keys())
+"""dict_keys(['name', 'roll_no', 'age', 'mark'])"""
