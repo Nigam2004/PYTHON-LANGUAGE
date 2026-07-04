d1fbd068fe075baf0ec73b9b26e1f99145553e1c
Nigam Bisoyi <nigambisoyi2004@gmail.com>
2026-07-04 10:41:17 +0530

    arr_prop

--- Numpy/fundamental/arr_prop.py
+++ Numpy/fundamental/arr_prop.py
@@ -7,19 +7,3 @@ print("Dimension:",arr.ndim)
 print("data Type:",arr.dtype)
 print("size:",arr.size)
 
-# Array Reshaping
-
-arr1=np.arange(12)
-print(arr1)
-
-reshape=arr.reshape((2,3))
-print("reshaped array: \n",reshape)
-
-flatened=reshape.flatten()  #it return copy
-print("flatened array: \n",flatened)
-
-raveled=flatened.ravel() 
-print("raveled array: \n",raveled)  #ravel returned vew insted of copy 
-
-transpose=reshape.T
-print("transpose array: \n",transpose)
