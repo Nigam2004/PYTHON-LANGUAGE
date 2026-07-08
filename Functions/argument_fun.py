11cefed50f31dd7862ead7433a34f675564d7a3a
Nigam Bisoyi <nigambisoyi2004@gmail.com>
2026-07-08 23:58:07 +0530

    changes

--- Functions/argument_fun.py
+++ Functions/argument_fun.py
@@ -4,7 +4,7 @@ def add(*number):
     for n in number:
         total+=n   
     return total
-    
-print(add(1,2,3))
+a=add(1,2,3)
+print(a)
 
 """Explantion=> On the above funcion *number act as tuplee ,all the argument are stored in form of tupple and user can perform any action related tuple """
