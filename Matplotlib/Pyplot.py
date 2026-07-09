974de5809fa655ef3076d39199a3bf767855730d
Nigam Bisoyi <nigambisoyi2004@gmail.com>
2026-07-09 18:17:37 +0530

    matplotlib

---
+++ Matplotlib/Pyplot.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+x_axis=np.array([0,6])
+y_axis=np.array([0,300])
+
+plt.plot(x_axis,y_axis)
+plt.show()
---
+++ Matplotlib/plotting.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+x_axis=np.array([1,3,5,7])
+y_axis=np.array([2,4,6,8])
+x__wxis=np.array([3, 8, 1, 10, 5, 7])
+
+# plt.plot(x_axis,y_axis,"*")
+# plt.plot(x_axis,y_axis,"o")
+plt.plot(x__wxis)  
+plt.show()
+
