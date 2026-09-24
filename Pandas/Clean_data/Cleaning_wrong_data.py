b6603bbdf296ecd385ed451049aaa5aaa48f512d
Nigam Bisoyi <nigambisoyi2004@gmail.com>
2026-09-24 13:42:07 +0530

    minor_changes

--- Pandas/Clean_data/Cleaning_wrong_data.py
+++ Pandas/Clean_data/Cleaning_wrong_data.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 df=pd.read_csv("Pandas/Clean_data/domy.csv")
 # df.loc[row_no, 'column_name'] = value
+print(df.loc[1,"Duration"])
 for x in df.index:
     if df.loc[x,"Duration"] >120:
       df.loc[x,"Duration"]=99
